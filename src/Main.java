80b75e1a821004c53ef44c6ecb8af07a05ab1a30
Solpostgit <begemot_off@mail.ru>
2023-08-22 21:47:17 +0300

    Added task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        task1();/*
-        task2();
+        task1();
+        task2();/*
         task3();
         task4();
         task5();*/
@@ -10,8 +10,17 @@ public class Main {
 
     public static void task1() {
         System.out.println("Task 1");
-        int [] arr = new int[5];
+        int[] arr = new int[5];
         arr[0] = 15;
         System.out.println(arr[0]);
+    }
+
+    public static void task2() {
+        System.out.println("Task 2");
+        int[] arr = new int[5];
+        arr[0] = 15;
+        int xArr = arr[0];
+        xArr+=2;
+        System.out.println(arr[0] + ", " + xArr);
     }
 }
