e5c1d1bfa33c3352ea501cf99220538a693006d5
Solpostgit <begemot_off@mail.ru>
2023-08-22 21:44:53 +0300

    Added task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();/*
+        task2();
+        task3();
+        task4();
+        task5();*/
+    }
+
+    public static void task1() {
+        System.out.println("Task 1");
+        int [] arr = new int[5];
+        arr[0] = 15;
+        System.out.println(arr[0]);
     }
 }
